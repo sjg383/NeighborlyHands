a59eeb860ce2d4ff7e6ba77e508903ee0d10b963
Skylar Griffis <sjg383@psu.edu>
2020-05-31 02:41:19 -0500

    Added a unit testing module. One of the tests currently fails due connection issues with the database

---
+++ NeighborlyHands.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NeighborlyHands;
+using NeighborlyHands.Controllers;
+using NeighborlyHands.Models;
+
+namespace NeighborlyHands.Test.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Edit()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Edit(3) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
