89b232f52e7f05977682a75237ff66d93796e9bf
Skylar Griffis <sjg383@psu.edu>
2020-06-05 16:59:00 -0500

    implemented login button and logic

--- NeighborlyHands/Models/User.cs
+++ NeighborlyHands/Models/User.cs
@@ -18,5 +18,6 @@ namespace NeighborlyHands.Models
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
     }
 }
